7b9752da06345c187b6f4812794598590e5f0324
AFuxy <zakygames701@gmail.com>
2024-12-14 01:46:48 +0000

    Fixed spelling

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const { ActivityType } = require('discord.js');
 
 client.on("ready", async () => {
     console.log(`Bot online as ${client.user.tag}!`);
-    client.user.setPresence({ activities: [{ name: "Give everyone a Star.", type: ActivityType.Custom }], status: "online" })
+    client.user.setPresence({ activities: [{ name: "Giving everyone a Star.", type: ActivityType.Custom }], status: "online" })
 });
